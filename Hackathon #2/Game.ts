c6d3d5cdf085cd3fc57d9102ee3c18eebae24583
Boch <118819662+LeBoch@users.noreply.github.com>
2024-02-05 14:24:23 +0100

    Update - IsOver -> boolean

--- Hackathon #2/Game.ts
+++ Hackathon #2/Game.ts
@@ -6,16 +6,16 @@ import { Drawer } from "./Drawer.js";
 export class Game {
 
     protected display: Display;
-    protected isOver: string;
+    protected isOver: boolean = false;
     protected level: number;
     protected objects: Point[];
     protected player: Player | undefined;
     protected players: Player[];
 
-    constructor(isOver: string, level: number, objects: Point[], players: Player[], x: number, y: number) {
+    constructor(level: number, objects: Point[], players: Player[], x: number, y: number) {
         
         this.level = level;
-        this.isOver = isOver;
+
         this.display = new Display(new Drawer(1,1,1))
         this.objects = objects;
         this.player = new Player("player-1", 'r', false, 'red', x, y);
---
+++ Hackathon #2/index.ts
@@ -0,0 +1,2 @@
+import { Game } from "./Game";
+let g = Game = new Game()
