f7e87e0c92af21aa5c32b0d7acf723856d2254ee
djamilssb <157473835+djamilssb@users.noreply.github.com>
2024-02-05 14:10:43 +0100

    g

--- Hackathon #2/Game.ts
+++ Hackathon #2/Game.ts
@@ -1,6 +1,7 @@
 import { Player } from "./Player.js";
 import { Point } from "./Point.js";
 import { Display } from "./Display.js";
+import { Drawer } from "./Drawer.js";
 
 export class Game {
 
@@ -15,7 +16,7 @@ export class Game {
         
         this.level = level;
         this.isOver = isOver;
-        this.display = new Display()
+        this.display = new Display(new Drawer(1,1,1))
         this.objects = objects;
         this.player = new Player("player-1", 'r', false, 'red', x, y);
         this.player = new Player("player-2", 'r', false, 'blue', x, y);
