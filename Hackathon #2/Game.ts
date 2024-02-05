9277218949994c5688878e1832f6c907cf588789
djamilssb <157473835+djamilssb@users.noreply.github.com>
2024-02-05 14:04:21 +0100

    change

--- Hackathon #2/Game.ts
+++ Hackathon #2/Game.ts
@@ -1,14 +1,18 @@
 import { Player } from "./Player.js";
 import { Point } from "./Point.js";
 import { Display } from "./Display.js";
+
 export class Game {
+
     protected display: Display;
     protected isOver: string;
     protected level: number;
     protected objects: Point[];
     protected player: Player | undefined;
     protected players: Player[];
+
     constructor(display: Display, isOver: string, level: number, objects: Point[], players: Player[], x: number, y: number) {
+        
         this.level = level;
         this.isOver = isOver;
         this.display = new Display()
@@ -16,7 +20,7 @@ export class Game {
         this.player = new Player("player-1", 'r', false, 'red', x, y);
         this.player = new Player("player-2", 'r', false, 'blue', x, y);
         this.players = players;
+
     }
-    
 
 }
