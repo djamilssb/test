201a0bf02fc010fd2a293f4badcb9cc36d02f7d6
Boch <118819662+LeBoch@users.noreply.github.com>
2024-02-06 12:22:22 +0100

    Update - Display / Add display with json

--- Hackathon #2/Display.ts
+++ Hackathon #2/Display.ts
@@ -1,32 +1,34 @@
+import { Activable } from "./Activable.js";
 import { Drawer } from "./Drawer.js";
 import { Game } from "./Game.js";
 import { Wall } from "./Wall.js";
+import { Level } from "./Level.js";
+import { Player } from "./Player.js";
 
 export class Display {
 
-    protected drawer : Drawer;
-    
-    constructor(drawer:Drawer) {
-        this.drawer = new Drawer(8,8,8)
-        }
-        
-        public (game: Game){
-            let player = game.getPlayers();
-            let wall = game.getObjects();
-            this.drawer.clear()
-            
-            for(let i = 0 ; i < player.length ; i++ ) {
-                this.drawer.drawCircle(player[i].getPointX(),player[i].getPointY(), player[i].getColor())
-            }
+    protected drawer: Drawer;
 
-            if(wall instanceof Wall)  {
-            for(let i = 0 ; i < wall.length ; i++){
-                this.drawer.drawRectangle(wall[i].getPointX(),wall[i].getPointY())
+    constructor(drawer: Drawer) {
+        this.drawer = new Drawer(8, 8, 8)
+    }
 
-            }
+    public draw(level: Level, players: Player[]) {
+        let data = level.getLevel()
+        this.drawer.clear()
+
+        for (let i = 0; i < players.length; i++) {
+            this.drawer.drawCircle(players[i].getPointX(), players[i].getPointY(), players[i].getColor())
         }
 
+        for (let i = 0; i < data.length; i++) {
+            if (data[i] == 'wall') this.drawer.drawRectangle(data[i][0], data[i][1], W);
+            if (data[i] instanceof Activable) this.drawer.drawRectangle(data[i].getPointX(), data[i].getPointY());
         }
+    }
+
+
+}
         
     
 }
